53ed02ded68745b9e5a8cfd57a724ab8c125465f
Anjal Doshi <anjaldoshi09@gmail.com>
2023-02-13 16:54:14 -0800

    Bump plugin version to 0.1.1

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -39,7 +39,7 @@ extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
 	info->apiVersion = PLUGIN_API_VER;
 	info->name = "OSC IO";
-	info->libVersion = "0.1.0";
+	info->libVersion = "0.1.1";
 	info->numPlugins = NUM_PLUGINS;
 }
 
